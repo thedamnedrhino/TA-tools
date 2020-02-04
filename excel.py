c9df9fea2da0058a5f483874dc16ac8e109c5254
fsbakhtiar <farzadsbakhtiar@gmail.com>
2020-02-04 11:06:13 -0800

    added main file to generate the formulas

---
+++ excel.py
@@ -0,0 +1,21 @@
+"""
+The command file for excel_formula_generator
+"""
+import argparse
+from excel_formula_generator import *
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser('generate excel cell formulas for grading')
+	parser.add_argument('command', choices=['total', 'summary'])
+	parser.add_argument('startcolumn')
+	parser.add_argument('endcolumn')
+	parser.add_argument('--exclude', nargs='+', required=False, default=[])
+	args = parser.parse_args()
+
+	columns = Columns(args.startcolumn, args.endcolumn, exclude=args.exclude).get()
+	fg = FormulaGenerator()
+	if args.command == 'total':
+		print(fg.generate_total(columns))
+	elif args.command == 'summary':
+		print(fg.generate_summary(columns))
